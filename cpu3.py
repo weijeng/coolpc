13621827e3fdfa1995153d84c48a4bb1242ec949
weijeng <protoss0906@gmail.com>
2020-08-30 14:23:42 +0800

    use re.sub() to replace string
    8/30 manual add new cpu score

--- cpu3.py
+++ cpu3.py
@@ -25,22 +25,16 @@ class coolpcSpider(scrapy.Spider):
 			line=row.extract()
 			try: print line
 			except: continue
-                        try: price = re.search('\$\d+', line).group()
-                        except: continue
-			if "R3" in line: line=line.replace("R3", "AMD Ryzen 3")
-			if "R5" in line: line=line.replace("R5", "AMD Ryzen 5")
-			if "R7" in line: line=line.replace("R7", "AMD Ryzen 7")
-			if "R9" in line: line=line.replace("R9", "AMD Ryzen 9")
-			if "TR" in line: line=line.replace("TR", "Threadripper")
-			if "TR2" in line: line=line.replace("TR2", "Threadripper")
+			try: price = re.search('\$\d+', line).group()
+			except: continue
+			line = re.sub(r'(AMD )?R(\d)', r'AMD Ryzen \2', line)
+			line = re.sub('TR', 'Threadripper', line)
+			line = re.sub('Intel i', 'Intel Core i', line)
 			if "G5400" in line: line=line.replace("G5400", "Gold G5400")
-			if "Intel i" in line: line=line.replace("Intel i","Intel Core i")
-			if "AMD AMD" in line: line=line.replace("AMD AMD", "AMD")
+			cpu = re.search('(Intel|AMD)[\s\-\w\+]+', line).group()
 			try: watt = re.search('[\/\)]\d+W\/?', line).group().strip("W/").replace("/","").replace(")","")
 			except: pass
-			cpu = re.search('(Intel|AMD)[\s\-\w\+]+', line).group()
 			if 'Intel Xeon E5-2620 V4' in cpu: watt = "85"
-                        if watt == None: continue
 			try: cores = re.search('\d+[^\w\s,]\/\d+[^\w,](GPU)?', line).group()
 			except AttributeError: cores = re.search('\d[^\w\s\"]{1,2}(\d[^\w]{1})?', line).group()
 			if dict_cpu.get(cpu) == None: dict_cpu[cpu] = cores, watt, int(price.strip("$"))
@@ -51,7 +45,18 @@ class coolpcSpider(scrapy.Spider):
 		i=2
 		for x, y in dict_cpu.items():
 			j=str(i)
-			ws.append([ x, y[0], y[1], y[2], "=VLOOKUP(A" + j + ",Geek_single!A:B,2,0)", "=VLOOKUP(A" + j + ",Geek_multiple!A:B,2,0)", "=(E"+j+"*2+F"+j+")/D"+j ])
+			s1 = "=VLOOKUP(A" + j + ",Geek_single!A:B,2,0)"
+			s2 = "=VLOOKUP(A" + j + ",Geek_multiple!A:B,2,0)"
+			sp_ratio = "=(E"+j+"*2+F"+j+")/D"+j
+			if "4350G" in x: s1 = 1137; s2 = 4954
+			if "4650G" in x: s1 = 1184; s2 = 6994
+			if "4750G" in x: s1 = 1239; s2 = 8228
+			if "G4930" in x: s1 = 810; s2= 1501
+			if "G5900" in x: s1 = 838; s2= 1431
+			if "10400F" in x: s1 = 1089; s2 = 6160
+			if "10700F" in x: s1 = 1265; s2 = 8090
+			if "10700KF" in x: s1 = 1414; s2 = 9098
+			ws.append([ x, y[0], y[1], y[2], s1, s2, sp_ratio ])
 			i+=1
 		wb.save(filename)
 		
