eb3f4b83af283553f002407a5248a5590c900aa8
weijeng <protoss0906@gmail.com>
2020-09-11 17:13:08 +0800

    add CL to ram.py

--- ram.py
+++ ram.py
@@ -19,28 +19,31 @@ class coolpcSpider(scrapy.Spider):
 	def parse(self, response):
 		wb = openpyxl.Workbook()
 		ws = wb.active
+		ws.append(["Name", "Size", "Type", "Latency", "Price"])
 		for row in response.xpath('//select/optgroup')[5].xpath('.//option'):
-			name=size=type=price=None
+			name=size=type=latency=price=None
 			result=row.extract()
 			try: print result
 			except: continue
-			name = re.search('^.+?,', result).group()
-			name = re.sub('<.+?>','',name)
-			if "D4" in name: name = name.replace("D4", "DDR4")
-			if "DDR4 -" in name: name = name.replace("DDR4 -","DDR4-")
-			if "DDR" not in name: continue
-			print "  ==> " + name
-			try: size = re.search('[\-\s]?\d+(GB|BG)(\*\d)?', name).group()
-			except: size = re.search('\d+G\(\d+G\*\d\)', name).group()
-			name = name.replace(size, "")
-			size = size.replace("BG","GB").replace(" ","")
-			type = re.search('DDR\dL?[\-\s](\d+|ECC)', name).group()
-			name = name.replace(type, "").replace(",","")
-			if "/" in name: name = name.replace("/","")
 			try: price = re.findall('\$\d+', result)[1]
 			except: price = re.findall('\$\d+', result)[0]
-			ws.append([name, size, type, price])
-			print "  ==> " + size + "\t" + type + "\t" + price
-			#time.sleep(2)
+			result = re.search('^.+?,', result).group()
+			result = re.sub('<.+?>','',result)
+			result = re.sub('(D4-|DDR4 -)','DDR4-',result)
+			if "DDR" not in result: continue
+			try: size = re.search('[\-\s]?\d+(GB|BG|G)(\*\d)?', result).group()
+			except: size = re.search('\d+G\(\d+G\*\d\)', result).group()
+			size = size.replace("BG","GB").replace(" ","")
+			type = re.search('DDR\dL?[\-\s](\d+|ECC)', result).group()
+			try: latency = re.search('CL ?\d+(-\d+-\d+)?', result).group()
+			except: pass
+			name = re.sub(size,'',result)
+			name = re.sub(type,'',name)
+			try: name = re.sub(latency,'',name)
+			except: pass
+			name = re.sub('[,/]','',name)
+			type = re.sub(' ','-',type)
+			print "  ==> " + name
+			ws.append([name, size, type, latency, price])
 		wb.save(filename)
 	
