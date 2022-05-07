3dd99d2dedacdc4180681bc404df5fa2077523f2
i5-11400 <protoss0906@gmail.com>
2022-05-07 16:53:05 +0800

    Modify cpu_ssd.py   add cpu score site (nanoreview)   add ssd pcie version

--- cpu_ssd.py
+++ cpu_ssd.py
@@ -11,10 +11,12 @@ class coolpcSpider(scrapy.Spider):
 	def start_requests(self):
 		urls = [
 			'http://www.coolpc.com.tw/evaluate.php',
-			'https://browser.geekbench.com/processor-benchmarks/'
+			'https://browser.geekbench.com/processor-benchmarks/',
+			'https://nanoreview.net/en/cpu-list/cinebench-scores'
 		]
 		yield scrapy.Request(url=urls[0], callback=self.parse)
 		yield scrapy.Request(url=urls[1], callback=self.parse_geek)
+		yield scrapy.Request(url=urls[2], callback=self.parse_nanoreview)
 
 	def parse(self, response):
 		wb = openpyxl.Workbook()
@@ -27,9 +29,11 @@ class coolpcSpider(scrapy.Spider):
 			line=row.extract()
 			try: print(line)
 			except: print(" ==> Can not print"); continue
-			if "ROG STRIX" in line: continue
-			if "+AMD" in line: continue
-			if "NT.$" in line: continue
+			if "技嘉" in line: continue
+			if "華碩" in line: continue
+			if "微星" in line: continue
+			if "金士頓" in line: continue
+			if "鍵盤" in line: continue
 			price = re.findall('\$\d+', line)
 			if len(price) > 1: price = price[1]
 			elif len(price) == 1: price = price[0]
@@ -52,12 +56,11 @@ class coolpcSpider(scrapy.Spider):
 		i=2
 		for x, y in dict_cpu.items():
 			j=str(i)
-			s1 = "=VLOOKUP(A" + j + ",Geek_single!A:B,2,0)"
-			s2 = "=VLOOKUP(A" + j + ",Geek_multiple!A:B,2,0)"
+			s1 = "=VLOOKUP(A" + j + ",nanoreview!A:C,2,0)"
+			s2 = "=VLOOKUP(A" + j + ",nanoreview!A:C,3,0)"
 			sp_ratio = "=(E"+j+"*2+F"+j+")/D"+j
-			if "G6400" in x: s1 = 1017; s2 = 2405
-			if "G6405" in x: s1 = 1074; s2 = 2417
-			if "10105F" in x: s1 = 1161; s2 = 4558
+			x = re.sub(" MPK", "", x)
+			x = re.sub(r'(\d{4})G PRO', r'PRO \1G', x)
 			ws.append([ x, y[0], y[1], y[2], s1, s2, sp_ratio ])
 			i+=1
 		wb.save(filename)
@@ -65,9 +68,9 @@ class coolpcSpider(scrapy.Spider):
 		
 		# SSD
 		ws = wb.create_sheet("SSD")
-		ws.append(["Name", "Size", "Read", "Write", "Type", "Price"])	
+		ws.append(["Name", "Size", "Read", "Write", "Type", "Price", "PCIe Ver"])	
 		for row in response.xpath('//select/optgroup')[6].xpath('.//option'):
-			name=size=read=write=type=price=None
+			name=size=read=write=type=price=pcie=None
 			result=row.extract()
 			try: print(result)
 			except: continue
@@ -75,6 +78,8 @@ class coolpcSpider(scrapy.Spider):
 			name = re.sub('<.+?>','',name)
 			name = name.replace("/", "")
 			name = re.sub('[^w]:\d+M','',name)
+			if "Gen4" in result: pcie="4.0"
+			if "P5 Plus" in result: pcie="4.0"
 			try: size = re.search('\d{3,4}GB?|\dTB', result).group()
 			except: pass
 			if size: 
@@ -89,7 +94,7 @@ class coolpcSpider(scrapy.Spider):
 			try: type = re.search('[TQM]LC', result).group()
 			except: pass
 			price = re.search('\$\d+', result).group()
-			ws.append([name, size, read, write, type, price])
+			ws.append([name, size, read, write, type, price, pcie])
 			#print "  ==> " + name + '\t' + size + '\t' + read + '\t' + write + '\t' + price
 		wb.save(filename)
 		time.sleep(3)
@@ -113,3 +118,20 @@ class coolpcSpider(scrapy.Spider):
 				score = 0
 		wb.save(filename)
 
+	def parse_nanoreview(self, response):
+		wb = openpyxl.load_workbook(filename)
+		ws = wb.create_sheet("nanoreview")
+		ws.append(["CPU", "Single-Core Score", "Multi-Core Score"])
+		cpu = response.xpath('//td/a/text()').extract()
+		score = response.xpath('//td/text()').extract()
+		single = []
+		multiple = []
+		for i in range(1, len(score), 8):
+			single.append(score[i].strip())
+			multiple.append(score[i+2].strip())
+		for x, y, z in zip(cpu, single, multiple):
+			x = re.sub(r'Core i(\d) ', r'Intel Core i\1-', x)
+			x = re.sub("Ryzen", "AMD Ryzen", x)
+			ws.append([x, y, z])
+		wb.save(filename)
+		
